e56db8d6bae37cc1938d4491f4c4dd1ba6d426c3
meshhi <yumythug@yandex.ru>
2023-10-15 22:29:20 +0300

    cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 const app = new Koa();
 
+app.use(cors());
 app.use(async ctx => {
   await new Promise(resolve => setTimeout(resolve, 2000));
   ctx.body = 'Hello World';
