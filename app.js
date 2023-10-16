60c30d6a62e76b25820cf5d4d998fa92a30ef546
meshhi <yumythug@yandex.ru>
2023-10-16 22:54:25 +0300

    json

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const Koa = require('koa');
 const cors = require('@koa/cors');
+const serve = require('koa-static')
+const path = require('path')
+const fs = require('fs');
 const app = new Koa();
 
+app.use(serve(path.join(__dirname, '/static')))
 app.use(cors());
 app.use(async ctx => {
   await new Promise(resolve => setTimeout(resolve, 2000));
-  ctx.body = 'Hello World';
+  ctx.body = JSON.stringify({
+    title: 'My title',
+    content: 'Its some description for test request'
+  });
 });
 
 app.listen(3000);
